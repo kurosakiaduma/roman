9433cde8a5690e1e78668aa0928ff617f17d73cc
Tevin Aduma <Tevin Aduma>
2017-02-22 18:19:48 +0300

    create new variables to store numeral arrays

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    $("form#word").submit(function(event) {
-        event.preventDefault();
 
-    });
+    var roman = new Array();
+    roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    var decimal = new Array();
+    decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-    var out = document.getElementById('result');
+
 });
