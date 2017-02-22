813da247b57429eab644d7fe262c64775901d693
Tevin Aduma <Tevin Aduma>
2017-02-22 18:22:01 +0300

    add a function to convert decimal numerals to roman numerals

--- js/script.js
+++ js/script.js
@@ -5,5 +5,21 @@ $(document).ready(function() {
     var decimal = new Array();
     decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
+    function getRoman(value) {
+        var romanNumeral = "";
+        var numThousands = 0;
+        for (var i = 0; i < roman.length; i++) {
+            if (value === 0) break;
+            while (value >= decimal[i]) {
+                value -= decimal[i];
+                romanNumeral += roman[i];
+                if (roman[i] === 'M') numThousands++;
+            }
+        }
+        return {
+            numThousands: numThousands,
+            romanNumeral: romanNumeral
+        };
+    }
 
 });
